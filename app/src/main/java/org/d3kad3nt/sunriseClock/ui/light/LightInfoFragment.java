b95d2a9f7fac8c19c81eb6943599ec1f36f94683
Christoph Wildhagen <git@christoph-wildhagen.de>
2021-05-14 22:30:53 +0200

    Added Simple switch to switch light state

--- app/src/main/java/org/d3kad3nt/sunriseClock/ui/light/LightInfoFragment.java
+++ app/src/main/java/org/d3kad3nt/sunriseClock/ui/light/LightInfoFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import org.d3kad3nt.sunriseClock.databinding.LightInfoFragmentBinding;
-import org.d3kad3nt.sunriseClock.data.remote.common.Status;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
 import org.d3kad3nt.sunriseClock.data.model.light.LightID;
+import org.d3kad3nt.sunriseClock.data.remote.common.Status;
+import org.d3kad3nt.sunriseClock.databinding.LightInfoFragmentBinding;
 
 public class LightInfoFragment extends Fragment {
 
@@ -24,16 +28,24 @@ public class LightInfoFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+                             @Nullable Bundle savedInstanceState){
         LightInfoFragmentBinding binding = LightInfoFragmentBinding.inflate(inflater, container, false);
         LightInfoFragmentArgs args = LightInfoFragmentArgs.fromBundle(requireArguments());
         LightID lightID = args.getLight();
         viewModel = new ViewModelProvider(requireActivity()).get(LightInfoViewModel.class);
         viewModel.getLight(lightID).observe(getViewLifecycleOwner(), baseLightResource -> {
-            if (baseLightResource.getStatus().equals(Status.SUCCESS)) {
+            if (baseLightResource.getStatus().equals(Status.SUCCESS)){
                 binding.setLight(baseLightResource.getData());
             }
         });
+
+        SwitchMaterial toggle = binding.switch1;
+        toggle.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                viewModel.setOn(lightID, isChecked);
+            }
+        });
+
         return binding.getRoot();
     }
 }
